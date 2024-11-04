2aae7a5bac370638de2537d3859118a741887b03
Muryllo Tiraza <muryllo.santos@docnix.com.br>
2024-11-03 23:07:48 -0300

    feature: Commit inicial

---
+++ api/src/main/java/br/com/subscontrol/SubsControl/SubsControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.subscontrol.SubsControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsControlApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/subscontrol/SubsControl/SubsControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.subscontrol.SubsControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
