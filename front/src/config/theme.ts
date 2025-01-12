fda4ea679784bbf38b44d1946f291549a9388be1
Muryllo Tiraza <muryllo.santos@docnix.com.br>
2025-01-11 23:16:49 -0300

    feat: Criando front em react

--- front/src/config/theme.ts
+++ front/src/config/theme.ts
@@ -1,31 +1,16 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material"
 
 export const appTheme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: "#f5f5f1",
-        },
-        secondary: {
-            main: "#E50914",
-        },
-        text: {
-            primary: "#f5f5f1",
-        },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#f5f5f1",
     },
-    typography: {
-        fontFamily: "Arial, sans-serif",
+    secondary: {
+      main: "#E50914",
     },
-    shape: {
-        borderRadius: 8,
+    text: {
+      primary: "#f5f5f1",
     },
-    breakpoints: {
-        values: {
-            xs: 0,
-            sm: 600,
-            md: 960,
-            lg: 1280,
-            xl: 1920,
-        },
-    },
-});
+  },
+})
